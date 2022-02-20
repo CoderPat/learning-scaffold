db764d514eff4a05ada0078c026148689e4c004c
Marcos Treviso <marcosvtreviso@gmail.com>
2022-02-20 06:34:58 -0400

    Add scripts to compute plausibility scores for mlqe

---
+++ notebooks/evaluate_explanations.py
@@ -0,0 +1,221 @@
+import argparse
+import os
+
+import numpy as np
+
+from sklearn.metrics import roc_auc_score, average_precision_score
+from scipy.stats import pearsonr, spearmanr
+
+
+def save_list(fpath, list_of_vals):
+    print('Saving to {}'.format(fpath))
+    with open(fpath, 'w') as f:
+        for i, v in enumerate(list_of_vals):
+            if isinstance(v, (int, float)):
+                s = '{}'.format(v)
+            else:
+                s = ' '.join(['{}'.format(v_) for v_ in v])
+            f.write(s + '\n')
+
+
+def validate_word_level_data(gold_explanations, model_explanations):
+    """
+    Ignore word level samples with full OK or full BAD tags
+    """
+    valid_gold, valid_model = [], []
+    for gold_expl, model_expl in zip(gold_explanations, model_explanations):
+        if sum(gold_expl) == 0 or sum(gold_expl) == len(gold_expl):
+            continue
+        else:
+            valid_gold.append(gold_expl)
+            valid_model.append(model_expl)
+    return valid_gold, valid_model
+
+
+def compute_auc_score(gold_explanations, model_explanations):
+    res = 0
+    for i in range(len(gold_explanations)):
+        res += roc_auc_score(gold_explanations[i], model_explanations[i])
+    return res / len(gold_explanations)
+
+
+def compute_ap_score(gold_explanations, model_explanations):
+    res = 0
+    for i in range(len(gold_explanations)):
+        res += average_precision_score(gold_explanations[i], model_explanations[i])
+    return res / len(gold_explanations)
+
+
+def compute_rec_topk(gold_explanations, model_explanations):
+    res = 0
+    for i in range(len(gold_explanations)):
+        idxs = np.argsort(model_explanations[i])[::-1][:int(sum(gold_explanations[i]))]
+        res += len([idx for idx in idxs if gold_explanations[i][idx] == 1])/sum(gold_explanations[i])
+    return res / len(gold_explanations)
+
+
+def evaluate_word_level(gold_explanations, model_explanations, verbose=True):
+    gold_explanations, model_explanations = validate_word_level_data(gold_explanations, model_explanations)
+    auc_score = compute_auc_score(gold_explanations, model_explanations)
+    ap_score = compute_ap_score(gold_explanations, model_explanations)
+    rec_topk = compute_rec_topk(gold_explanations, model_explanations)
+    if verbose:
+        print('AUC score: {:.4f}'.format(auc_score))
+        print('AP score: {:.4f}'.format(ap_score))
+        print('Recall at top-K: {:.4f}'.format(rec_topk))
+    return auc_score, ap_score, rec_topk
+
+
+def evaluate_sentence_level(gold_scores, model_scores, verbose=True):
+    y_gold = np.array(gold_scores)
+    y_hat = np.array(model_scores)
+    pearson = pearsonr(y_gold, y_hat)[0]
+    spearman = spearmanr(y_gold, y_hat)[0]
+    mae = np.abs(y_gold - y_hat).mean()
+    rmse = ((y_gold - y_hat) ** 2).mean() ** 0.5
+    if verbose:
+        print("Pearson: {:.4f}".format(pearson))
+        print("Spearman: {:.4f}".format(spearman))
+        print("MAE: {:.4f}".format(mae))
+        print("RMSE: {:.4f}".format(rmse))
+    return pearson, spearman, mae, rmse
+
+
+def aggregate_pieces(x, mask, reduction='first'):
+    import torch
+    """
+    :param x: tensor of shape (seq_len) or (seq_len, hdim)
+    :param mask: bool tensor of shape (seq_len)
+    :param reduction: aggregation strategy (first, max, sum, mean)
+
+    :returns: <s> word_1 word_2 ... </s>
+    where word_i = aggregate(piece_i_1, piece_i_2, ...)
+    """
+    # mark <s> and </s> as True
+    special_mask = mask.clone()
+    special_mask[0] = special_mask[-1] = True
+    if reduction == 'first':
+        return x[special_mask.bool()]
+    elif reduction == 'sum' or reduction == 'mean' or reduction == 'max':
+        idx = special_mask.long().cumsum(dim=-1) - 1
+        idx_unique_count = torch.bincount(idx)
+        num_unique = idx_unique_count.shape[-1]
+        if reduction == 'sum':
+            res = torch.zeros(num_unique, device=x.device, dtype=x.dtype).scatter_add(0, idx, x)
+        elif reduction == 'mean':
+            res = torch.zeros(num_unique, device=x.device, dtype=x.dtype).scatter_add(0, idx, x)
+            res /= idx_unique_count.float()
+        else:
+            res = torch.stack([x[idx == i].max() for i in range(num_unique)]).to(x.device)
+        return res.float()
+
+    else:
+        return x
+
+
+def sigmoid(x):
+    return 1 / (1+np.exp(-x))
+
+
+def softmax(x):
+    return np.exp(x) / np.exp(x).sum()
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--gold_explanations_fname_src', type=str, required=True)
+    parser.add_argument('--gold_explanations_fname_mt', type=str, required=True)
+    parser.add_argument('--model_explanations_fname_src', type=str, required=True)
+    parser.add_argument('--model_explanations_fname_mt', type=str, required=True)
+    parser.add_argument('--gold_sentence_scores_fname', type=str, required=True)
+    parser.add_argument('--model_sentence_scores_fname', type=str, required=True)
+    parser.add_argument('--model_fp_mask_mt', type=str, default=None, required=False)
+    parser.add_argument('--model_fp_mask_src', type=str,  default=None, required=False)
+    parser.add_argument('-r', '--reduction', default='sum', choices=['none', 'first', 'sum', 'mean', 'max'])
+    parser.add_argument('-t', '--transform', default='pre', choices=['none', 'pre', 'pos', 'pre_neg'])
+    parser.add_argument('-i', '--idxs', type=str, default=None, help='idxs for the dev subset')
+    args = parser.parse_args()
+    src_gold_explanations, src_model_explanations = read_word_data(args.gold_explanations_fname_src,
+                                                                   args.model_explanations_fname_src)
+    mt_gold_explanations, mt_model_explanations = read_word_data(args.gold_explanations_fname_mt,
+                                                                 args.model_explanations_fname_mt)
+    gold_scores, model_scores = read_sentence_data(args.gold_sentence_scores_fname, args.model_sentence_scores_fname)
+    if args.model_fp_mask_src is not None:
+        src_fp_mask = read_fp_mask_data(args.model_fp_mask_src)
+    else:
+        src_fp_mask = [[1] for _ in range(len(gold_scores))]
+    if args.model_fp_mask_mt is not None:
+        mt_fp_mask = read_fp_mask_data(args.model_fp_mask_mt)
+    else:
+        mt_fp_mask = [[1] for _ in range(len(gold_scores))]
+    is_rembert = 'rembert' in args.model_explanations_fname_mt
+
+    if args.idxs is not None:
+        idxs = read_idxs(args.idxs)
+        src_gold_explanations = [src_gold_explanations[i] for i in idxs]
+        src_model_explanations = [src_model_explanations[i] for i in idxs]
+        mt_gold_explanations = [mt_gold_explanations[i] for i in idxs]
+        mt_model_explanations = [mt_model_explanations[i] for i in idxs]
+        src_fp_mask = [src_fp_mask[i] for i in idxs]
+        mt_fp_mask = [mt_fp_mask[i] for i in idxs]
+        gold_scores = [gold_scores[i] for i in idxs]
+        model_scores = [model_scores[i] for i in idxs]
+
+    # hack to fix transquest attn expls since the input is: <s> <src> <mt> </s> instead of <s> <mt> <src> </s>
+    if 'transquest' in args.model_explanations_fname_mt and 'attn' in args.model_explanations_fname_mt:
+        src_model_explanations, mt_model_explanations = mt_model_explanations, src_model_explanations
+        src_fp_mask, mt_fp_mask = mt_fp_mask, src_fp_mask
+
+    # scores for each word and also for <s> and </s>
+    for i in range(len(src_model_explanations)):
+        src_model_explanations[i] = torch.tensor(src_model_explanations[i])
+        mt_model_explanations[i] = torch.tensor(mt_model_explanations[i])
+        src_fp_mask[i] = torch.tensor(src_fp_mask[i])
+        mt_fp_mask[i] = torch.tensor(mt_fp_mask[i])
+
+        if args.transform == 'pre':
+            src_model_explanations[i] = torch.sigmoid(torch.abs(src_model_explanations[i]))
+            mt_model_explanations[i] = torch.sigmoid(torch.abs(mt_model_explanations[i]))
+
+        if args.transform == 'pre_neg':
+            src_model_explanations[i] = - src_model_explanations[i]
+            mt_model_explanations[i] = - mt_model_explanations[i]
+
+        src_model_explanations[i] = aggregate_pieces(src_model_explanations[i], src_fp_mask[i], args.reduction)
+        mt_model_explanations[i] = aggregate_pieces(mt_model_explanations[i], mt_fp_mask[i], args.reduction)
+
+        if args.transform == 'pos':
+            src_model_explanations[i] = torch.sigmoid(torch.abs(src_model_explanations[i]))
+            mt_model_explanations[i] = torch.sigmoid(torch.abs(mt_model_explanations[i]))
+
+        # remove <s> and </s>
+        a = 0 if args.reduction == 'none' else 1
+        b = None if args.reduction == 'none' else -1
+        src_model_explanations[i] = src_model_explanations[i][a:b].cpu().tolist()
+        a = 0 if is_rembert else a
+        mt_model_explanations[i] = mt_model_explanations[i][a:b].cpu().tolist()
+
+    print('Saving aggregated explanations...')
+    dname = os.path.dirname(args.model_explanations_fname_src)
+    save_list(os.path.join(dname, 'aggregated_source_scores.txt'), src_model_explanations)
+    dname = os.path.dirname(args.model_explanations_fname_mt)
+    save_list(os.path.join(dname, 'aggregated_mt_scores.txt'), mt_model_explanations)
+
+    print('-----------------')
+    print('Source word-level')
+    src_auc_score, src_ap_score, src_rec_topk = evaluate_word_level(src_gold_explanations, src_model_explanations)
+    print('-----------------')
+    print('MT word-level')
+    mt_auc_score, mt_ap_score, mt_rec_topk = evaluate_word_level(mt_gold_explanations, mt_model_explanations)
+    print('-----------------')
+    print('Sentence-level')
+    corr = evaluate_sentence_level(gold_scores, model_scores)
+
+    # to google docs
+    print('{:.4f}\t{:.4f}\t{:.4f}\t{:.4f}\t{:.4f}\t{:.4f}'.format(
+        mt_auc_score, mt_ap_score, mt_rec_topk, src_auc_score, src_ap_score, src_rec_topk
+    ))
+
+
+if __name__ == '__main__':
+    main()
---
+++ scripts/calc_plausiblity_mlqe.py
@@ -0,0 +1,224 @@
+import sys
+import argparse
+import numpy as np
+import jax.numpy as jnp
+from functools import partial
+from entmax_jax.activations import sparsemax, entmax15
+from sklearn.metrics import roc_auc_score, average_precision_score
+from transformers import XLMRobertaTokenizerFast
+
+from meta_expl.explainers import load_explainer
+from meta_expl.models import load_model
+from meta_expl.data.mlqe import dataloader
+
+
+# gambi
+sys.path.append("../notebooks/")
+sys.path.append("./notebooks/")
+from evaluate_explanations import (
+    evaluate_word_level,
+    evaluate_sentence_level,
+    aggregate_pieces,
+)
+from utils import aggregate_pieces
+
+
+def unroll(list_of_lists):
+    return [e for ell in list_of_lists for e in ell]
+
+
+def read_data(lp, split="dev"):
+    def tags_to_ints(line):
+        return list(map(int, line.strip().replace("OK", "0").replace("BAD", "1").split()))
+
+    data = {
+        "original": [line.strip() for line in open("notebooks/data/{}/{}.src".format(lp, split), "r")],
+        "translation": [line.strip() for line in open("notebooks/data/{}/{}.mt".format(lp, split), "r")],
+        "da": [float(line.strip()) for line in open("notebooks/data/{}/{}.da".format(lp, split), "r")],
+        "src_tags": [tags_to_ints(line) for line in open("notebooks/data/{}/{}.src-tags".format(lp, split), "r")],
+        "mt_tags": [tags_to_ints(line) for line in open("notebooks/data/{}/{}.tgt-tags".format(lp, split), "r")],
+    }
+    z = np.array(data["da"])
+    data["z_mean"] = (z - z.mean()) / z.std()
+    data = [dict(zip(data.keys(), v)) for v in list(zip(*data.values()))]
+    return data
+
+
+def get_src_and_mt_explanations(all_tokens, all_fp_masks, all_explanations, reduction):
+    import torch
+
+    src_expls = []
+    mt_expls = []
+    src_pieces = []
+    mt_pieces = []
+    for tokens, expl, fp_mask in zip(all_tokens, all_explanations, all_fp_masks):
+        # split data into src and mt (assuming "<s> src </s> mt </s>" format without CLS for mt)
+        src_len = tokens.index(tokenizer.sep_token) + 1
+        src_tokens, mt_tokens = tokens[:src_len], tokens[src_len:]
+        src_expl, mt_expl = expl[:src_len], expl[src_len:]
+        src_fp_mask, mt_fp_mask = fp_mask[:src_len], fp_mask[src_len:]
+
+        # aggregate word pieces scores (use my old good torch function)
+        agg_src_expl = aggregate_pieces(torch.tensor(src_expl), torch.tensor(src_fp_mask), reduction)
+        agg_mt_expl = aggregate_pieces(torch.tensor(mt_expl), torch.tensor(mt_fp_mask), reduction)
+
+        # remove <s> and </s> from src
+        agg_src_expl = agg_src_expl.tolist()[1:-1]
+        # remove </s> from mt
+        agg_mt_expl = agg_mt_expl.tolist()[:-1]
+
+        src_pieces.append(src_tokens)
+        mt_pieces.append(mt_tokens)
+        src_expls.append(agg_src_expl)
+        mt_expls.append(agg_mt_expl)
+    return src_expls, mt_expls, src_pieces, mt_pieces
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    # 'data/mlqe-xlmr-explainer/teacher_dir'
+    # 'data/mlqe-xlmr-explainer/teacher_expl_dir'
+    parser.add_argument("--teacher-dir", type=str, required=True)
+    parser.add_argument("--explainer-dir", type=str, required=True)
+    parser.add_argument("--tokenizer", type=str, default="xlm-roberta-base")
+    parser.add_argument("--lp", type=str, default="ro-en")
+    parser.add_argument("--max-len", type=int, default=256)
+    parser.add_argument("--batch-size", type=int, default=16)
+    parser.add_argument("--task-type", type=str, default="regression")
+    parser.add_argument("--use-top-layer-head", action='store_true')
+    args = parser.parse_args()
+
+    # arguments
+    teacher_dir = args.teacher_dir
+    explainer_dir = args.explainer_dir
+    tokenizer_arch = args.tokenizer
+    lp = args.lp
+    max_len = args.max_len
+    batch_size = args.batch_size
+    sep_token = "</s>" if "xlm" in tokenizer_arch else "[SEP]"
+    num_classes = 1
+    task_type = "regression"
+
+    # create dummy inputs for model instantiation
+    input_ids = jnp.ones((batch_size, max_len), jnp.int32)
+    dummy_inputs = {
+        "input_ids": input_ids,
+        "attention_mask": jnp.ones_like(input_ids),
+        "token_type_ids": jnp.arange(jnp.atleast_2d(input_ids).shape[-1]),
+        "position_ids": jnp.ones_like(input_ids),
+    }
+    dummy_inputs["input_ids"].shape
+
+    # load validation data
+    print("loading data...")
+    dataloader = partial(dataloader, sep_token=sep_token)
+    valid_data = read_data(lp, "dev")
+
+    # load tokenizer
+    print("loading tokenizer...")
+    tokenizer = XLMRobertaTokenizerFast.from_pretrained(tokenizer_arch)
+    cls_id = tokenizer.cls_token_id
+    sep_id = tokenizer.sep_token_id
+    pad_id = tokenizer.pad_token_id
+
+    # load models
+    print("loading teacher and its explainer...")
+    teacher, teacher_params, dummy_state = load_model(teacher_dir, batch_size, max_len)
+    teacher_explainer, teacher_explainer_params = load_explainer(explainer_dir, dummy_inputs, state=dummy_state)
+
+    # auxiliary function to get the explanations
+    def get_explanations(data, use_top_layer_head=False):
+        all_tokens = []
+        all_masks = []
+        all_explanations = []
+        all_outputs = []
+        c = 1
+        for x, y in dataloader(data, tokenizer, batch_size=batch_size, max_len=max_len, shuffle=False):
+            print("{} of {}".format(c, len(data) // batch_size + 1), end="\r")
+            c += 1
+
+            # get teacher output
+            y_teacher, teacher_attn = teacher.apply(teacher_params, **x, deterministic=True)
+            if task_type == "classification":
+                y_teacher = jnp.argmax(y_teacher, axis=-1)
+
+            # get explanation from the teacher explainer
+            teacher_expl, _ = teacher_explainer.apply(teacher_explainer_params, x, teacher_attn)
+
+            if use_top_layer_head:
+                # batch x layers x heads x seqlen x seqlen
+                all_attentions = jnp.stack(teacher_attn['attentions']).transpose([1, 0, 2, 3, 4])
+                num_layers = all_attentions.shape[1] 
+                num_heads = all_attentions.shape[2]
+
+                # get the attention from the teacher associated with the top head coeff
+                head_coeffs = sparsemax(teacher_explainer_params['params']['head_coeffs'])
+                top_joint_id = jnp.argmax(head_coeffs).item()
+                top_layer_id = top_joint_id // num_heads
+                top_head_id = top_joint_id % num_heads
+                attn = all_attentions[:, top_layer_id, top_head_id]
+                mask = x['attention_mask']
+                teacher_expl = (attn * mask[:, :, None]).sum(-2) / mask.sum(-1)[:, None]
+            
+            if False:
+                # get all attentions from the teacher associated with nonzero head coeff
+                head_coeffs = head_coeffs.reshape(num_layers, num_heads)
+                nonzero_rows, nonzero_cols = head_coeffs.nonzero()
+                num_nonzero = len(nonzero_rows)
+                attn = jnp.stack([
+                    all_attentions[:, r, c] for r, c in zip(nonzero_rows.tolist(), nonzero_cols.tolist())
+                ]).transpose([1, 0, 2, 3])  # batch, num_nonzero, seqlen, seqlen
+                mask = x['attention_mask']
+                teacher_expl = (attn * mask[..., None]).sum(-2) / mask.sum(-1)[..., None]
+
+            # convert everything to lists
+            batch_ids = x["input_ids"].tolist()
+            batch_tokens = [tokenizer.convert_ids_to_tokens(ids) for ids in batch_ids]
+            batch_masks = [[tk.startswith("▁") for tk in tokens] for tokens in batch_tokens]
+            batch_expls = teacher_expl.tolist()
+
+            # filter out pad
+            batch_valid_len = x["attention_mask"].sum(-1).tolist()
+            for i in range(len(batch_valid_len)):
+                n = batch_valid_len[i]
+                batch_ids[i] = batch_ids[i][:n]
+                batch_tokens[i] = batch_tokens[i][:n]
+                batch_masks[i] = batch_masks[i][:n]
+                batch_expls[i] = batch_expls[i][:n]
+
+            all_tokens.extend(batch_tokens)
+            all_masks.extend(batch_masks)
+            all_explanations.extend(batch_expls)
+            all_outputs.extend(y_teacher.tolist())
+
+        return all_tokens, all_masks, all_explanations, all_outputs
+
+    # get explanations for the dev set
+    print("getting explanations...")
+    valid_tokens, valid_masks, valid_explanations, valid_outputs = get_explanations(
+        valid_data, use_top_layer_head=args.use_top_layer_head
+    )
+
+    # split explanations int src and mt and aggregate word pieces scores
+    print("merging word pieces...")
+    reduction = "sum"
+    src_expls, mt_expls, src_pieces, mt_pieces = get_src_and_mt_explanations(
+        valid_tokens, valid_masks, valid_explanations, reduction=reduction
+    )
+
+    # evaluate predictions
+    gold_src_tokens = [inp["original"].split() for inp in valid_data]
+    gold_mt_tokens = [inp["translation"].split() for inp in valid_data]
+    gold_expls_src = [inp["src_tags"] for inp in valid_data]
+    gold_expls_mt = [inp["mt_tags"] for inp in valid_data]
+    gold_scores = [inp["z_mean"] for inp in valid_data]
+    pred_expls_src = src_expls
+    pred_expls_mt = mt_expls
+    pred_scores = unroll(valid_outputs)
+
+    print("Sentence-level results:")
+    evaluate_sentence_level(gold_scores, pred_scores)
+    print("Word-level SRC results:")
+    evaluate_word_level(gold_expls_src, pred_expls_src)
+    print("Word-level MT results:")
+    evaluate_word_level(gold_expls_mt, pred_expls_mt)
