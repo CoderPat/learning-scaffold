Fix gradient for attention explainers

smat/explainers/attention_attribution_explainer.py
@@ -10,7 +10,10 @@ class AttentionAttributionExplainer(SaliencyExplainer):
     """
 
     temperature: float = 0.1
+    n_steps: int = 10
     ord: int = 0
+    layer_idx: int = None
+    gradient_wrt = 'attention'
 
     def logit_computation(self, inputs, state, grad_fn, **model_extras):
         """
@@ -27,18 +30,33 @@ class AttentionAttributionExplainer(SaliencyExplainer):
             if self.layer_idx is None
             else all_attentions[self.layer_idx]
         )
+        baselines = jnp.zeros_like(head_attentions)
         word_embeddings = state["hidden_states"][0]
-        baselines = jnp.zeros_like(word_embeddings)
-
         y = jnp.argmax(state["outputs"], axis=-1)
-        grads = jnp.zeros_like(word_embeddings)
+
+        grads = jnp.zeros_like(head_attentions)
         for i in range(self.n_steps):
             # compute rienman delta (halved in the edges)
             delta = 1 / (self.n_steps * (2 - (jnp.minimum(i % (self.n_steps - 1), 1))))
             # compute rienman area and add to total integral estimate
             head_attentions = baselines + (head_attentions - baselines) * (i / (self.n_steps - 1)) * delta
-            # TODO: implement grad_fn w.r.t. attention probabilities
-            grads = grads + grad_fn(word_embeddings, head_attentions, y) * delta
+            if self.layer_idx is None:
+                bs, lxh, n, n = head_attentions.shape
+                num_layers = len(all_attentions)
+                head_attentions_l = head_attentions.reshape(bs, num_layers, -1, n, n)
+                head_gradients = [grad_fn(head_attentions_l[:, l], word_embeddings, y) for l in range(num_layers)]
+                head_gradients = jnp.concatenate(head_gradients, axis=1)
+            else:
+                head_gradients = grad_fn(head_attentions, word_embeddings, y)
+            grads = grads + head_gradients * delta
 
-        ret = head_attentions * grads
+        # summarize gradient for each query
+        if self.ord > 0:
+            ret = jnp.linalg.norm(grads * head_attentions, self.ord, axis=-1)
+        else:
+            ret = jnp.sum(grads * head_attentions, axis=-1)
+
+        # average all heads from all layers
+        ret = jnp.mean(ret, axis=1)
+
         return ret / self.temperature

smat/explainers/attention_times_gradient_explainer.py
@@ -10,7 +10,9 @@ class AttentionTimesGradientExplainer(SaliencyExplainer):
     """
 
     temperature: float = 1
+    layer_idx: int = None
     ord: int = 0
+    gradient_wrt = 'attention'
 
     def logit_computation(self, inputs, state, grad_fn, **model_extras):
         """
@@ -30,7 +32,19 @@ class AttentionTimesGradientExplainer(SaliencyExplainer):
         word_embeddings = state["hidden_states"][0]
         y = jnp.argmax(state["outputs"], axis=-1)
 
-        # TODO: implement grad_fn w.r.t. attention probabilities
-        grads = grad_fn(word_embeddings, head_attentions, y,)
-        ret = grads * head_attentions
+        if self.layer_idx is None:
+            head_gradients = [grad_fn(a, word_embeddings, y) for a in all_attentions]
+            head_gradients = jnp.concatenate(head_gradients, axis=1)
+        else:
+            head_gradients = grad_fn(all_attentions[self.layer_idx], word_embeddings, y)
+
+        # summarize gradient for each query
+        if self.ord > 0:
+            ret = jnp.linalg.norm(head_gradients * head_attentions, self.ord, axis=-1)
+        else:
+            ret = jnp.sum(head_gradients * head_attentions, axis=-1)
+
+        # average all heads from all layers
+        ret = jnp.mean(ret, axis=1)
+
         return ret / self.temperature

smat/models/roberta.py
@@ -84,7 +84,7 @@ class RobertaModel(WrappedModel):
         def model_fn(word_embeddings, y):
             _, hidden_states, _ = self.roberta_module.roberta.encoder(
                 word_embeddings,
-                inputs["attention_mask"],
+                attention_mask=inputs["attention_mask"],
                 head_mask=None,
                 output_hidden_states=True,
                 output_attentions=True,
@@ -104,15 +104,43 @@ class RobertaModel(WrappedModel):
 
         return jax.grad(model_fn)
 
+    # define gradient over attention
+    def attention_grad_fn(
+            self,
+            inputs,
+    ):
+        def model_fn(attn_weights, word_embeddings, y):
+            input_ids = inputs["input_ids"]
+            attention_mask = inputs["attention_mask"]
+
+            _, hidden_states, _ = self.roberta_module.roberta.encoder(
+                word_embeddings,
+                attention_mask,
+                head_mask=None,
+                attn_weights=attn_weights,
+                output_hidden_states=True,
+                output_attentions=True,
+                unnorm_attention=True,
+                deterministic=True,
+                return_dict=False,
+            )
+            outputs = self.scalarmix(hidden_states, attention_mask)
+            outputs = self.roberta_module.classifier(
+                outputs[:, None, :], deterministic=True
+            )
+            # we use sum over batch dimension since
+            # we need batched gradient and because embeddings
+            # on each sample are independent
+            # summing will just retrieve the batched gradient
+            return jnp.sum(outputs[jnp.arange(outputs.shape[0]), y], axis=0)
+
+        return jax.grad(model_fn)
+
     @staticmethod
     def extract_embeddings(params):
         return (
-            params["params"]["FlaxRobertaForSequenceClassificationModule_0"]["roberta"][
-                "embeddings"
-            ]["word_embeddings"]["embedding"],
-            params["params"]["FlaxRobertaForSequenceClassificationModule_0"]["roberta"][
-                "embeddings"
-            ]["position_embeddings"]["embedding"],
+            params['params']['roberta_module']['roberta']['embeddings']['word_embeddings']['embedding'],
+            params['params']['roberta_module']['roberta']['embeddings']["position_embeddings"]["embedding"],
         )
 
     @staticmethod
@@ -182,7 +210,7 @@ class RobertaModel(WrappedModel):
             value_layer = self.roberta_module.roberta.encoder.layer.layers[i].attention.self.value
             head_dim = self.config.hidden_size // self.config.num_attention_heads
             value_states = value_layer(hidden_state_layer).reshape(
-                hidden_states.shape[:2] + (self.config.num_attention_heads, head_dim)
+                hidden_state_layer.shape[:2] + (self.config.num_attention_heads, head_dim)
             )
             values.append(value_states)
         return values